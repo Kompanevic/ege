40c86d6b550ad076b6580786f297c7c2f4550242
kompanevic <dav.kompanevich@gmail.com>
2026-01-11 15:51:47 +0300

    поменял на 0

--- kege/2_shastin2.py
+++ kege/2_shastin2.py
@@ -3,5 +3,5 @@ for x in range(0,2):
     for y in range(0, 2):
         for z in range(0, 2):
             for w in range(0, 2):
-                if ((y <= x) and (not(z <= (x == w)))) == 1:
+                if ((y <= x) and (not(z <= (x == w)))) == 0:
                     print(x,y,z,w)
